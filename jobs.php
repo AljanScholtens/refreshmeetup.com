6e50c3a05646504116f5fc57f8a62ce52d49e112
Aljan Scholtens <aljan@scholtens.me>
2017-03-30 10:34:32 +0200

    Added jobs page

---
+++ jobs.php
@@ -0,0 +1,50 @@
+<?php include 'header.php'; ?>
+
+<h2 class="heading--h1">Jobs</h2>
+<p>Looking for a job? We're keeping track of design &amp; front-end jobs near Groningen.</p>
+
+<h3 class="u-uppercase">New</h3>
+
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705704/" class="o-card__link">
+    <span class="o-card__time">Concept7</span>
+    <span class="o-card__event">Interface designer</span>
+  </a>
+</div>
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705708/" class="o-card__link">
+    <span class="o-card__time">Catawiki</span>
+    <span class="o-card__event">Front-end developer</span>
+  </a>
+</div>
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705708/" class="o-card__link">
+    <span class="o-card__time">Payt</span>
+    <span class="o-card__event">Design system lead</span>
+  </a>
+</div>
+
+<br/>
+
+<h3 class="u-uppercase">Other</h3>
+
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705704/" class="o-card__link">
+    <span class="o-card__time">Concept7</span>
+    <span class="o-card__event">Interface designer</span>
+  </a>
+</div>
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705708/" class="o-card__link">
+    <span class="o-card__time">Catawiki</span>
+    <span class="o-card__event">Front-end developer</span>
+  </a>
+</div>
+<div class="o-card o-card--white o-card--row o-card--clickable">
+  <a href="https://www.meetup.com/refresh-groningen/events/233705708/" class="o-card__link">
+    <span class="o-card__time">Payt</span>
+    <span class="o-card__event">Design system lead</span>
+  </a>
+</div>
+
+<?php include 'footer.php'; ?>
